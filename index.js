03bd8e2e94ac56e73bd369cff89f94cf4ad980f6
tanmoyPaul12 <tanmoy01paul@gmail.com>
2026-01-14 13:02:28 +0530

    node  or nodemon

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import { load } from "cheerio";
+
+const app = express()
+const PORT = 4000
+
+const url = "https://www.theguardian.com/uk";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.listen(PORT, ()=> console.log(`server running on PORT ${PORT}`))
